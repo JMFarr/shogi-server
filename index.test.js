1ebf0b967a458aa375cd0fbb9d2c6691fda6bc4d
Jacob Farr <jacobmfarr@gmail.com>
2017-11-12 17:18:57 -0700

    Update index.test.js
    Removed a broken test.

--- index.test.js
+++ index.test.js
@@ -1,7 +1,1 @@
-const server = require('./server');
 
-test('a test', () => {
-	const mockDepenecyInjection = jest.fn();
-	server(mockDepenecyInjection);
-	expect(mockDepenecyInjection.mock.calls.length).toBe(2);
-});
