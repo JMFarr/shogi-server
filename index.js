d5c5aa79e7469b8d8323ed5521ced3f4d3d3b96e
Jacob Farr <jacobmfarr@gmail.com>
2017-11-12 18:15:52 -0700

    Set up a basic express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+
+const SERVER_PORT = 3000;
+
+const handleRequest = (req,res) => {
+	res.send("Hello, world!");
+}
+
+app.get('/', handleRequest);
+
+app.listen(SERVER_PORT, () => console.log("listening on port ", SERVER_PORT));
